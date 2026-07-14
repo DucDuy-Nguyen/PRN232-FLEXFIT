44f0638c20b5ea361ff6112bdc54e2ae7a051624
ng-dhp <duynguyen17122004@gmail.com>
2026-07-14 23:34:11 +0700

    Flexfit

--- obj/Release/net8.0/Flexfit.AssemblyInfo.cs
+++ obj/Release/net8.0/Flexfit.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flexfit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a05af98acb0dfe33d9355026d8f186369c55d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53973771ca1730ee98a273ca944f0bfb64bbfc3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flexfit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flexfit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
